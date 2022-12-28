266526390b724632798e65147d57e7accc11d84a
thanhdat2002 <80498296+thanhdat2002@users.noreply.github.com>
2022-12-28 15:59:23 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'BÉ BEO';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'HAPPY BIRTHDAY';
+const titleCard = 'Dear đến bạnn Như';
+const contentCard = 'Gữi tất cả lời chúc tốt đẹp đến bạnn Như.Mong bạn luôn học giỏi thành giáo viên tốt';
+
+
+const giftImage = 'nhu.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
